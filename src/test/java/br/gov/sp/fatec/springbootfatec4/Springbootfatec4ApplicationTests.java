2a07e2f47b265e9948797929ec0c34e5b0b2f0f1
Joaovsnas <166765258+Joaovsnas@users.noreply.github.com>
2025-09-18 10:06:33 -0300

    Add files via upload

---
+++ src/test/java/br/gov/sp/fatec/springbootfatec4/Springbootfatec4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springbootfatec4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootfatec4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
